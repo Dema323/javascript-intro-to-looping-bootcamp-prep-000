03e49de1649787c3c1547105c43be166317cdb69
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-22 14:27:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,3 +6,23 @@ function forLoop(){
   }
   }
 }
+
+function whileLoop(n){
+while(n>0){
+  console.log(--n)
+}
+return "done"
+  }
+}
+
+
+
+function doWhileLoop(){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do{
+    array +array.slice(1)
+  }while (array.length > 0 && maybeTrue())
+  return array
+}
